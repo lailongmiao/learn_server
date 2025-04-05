582c853fa52aeac877442bf0ca9539be42793327
lailongmiao <2969314242@qq.com>
2025-04-05 21:14:04 +0800

    Add insert data sql code to the migration script

--- migrations/20250405114501_create_users_table.sql
+++ migrations/20250405114501_create_users_table.sql
@@ -1,1 +1,7 @@
 -- Add migration script here
+-- create table users(
+--     id serial primary key,
+--     username varchar(255) not null unique,
+--     email varchar(255) not null unique,
+-- );
+ INSERT INTO users  (id,username,email) values(3,'test3','test3@example.com');
