c6321d6004835396270eb66008d8a532f97f4ba7
lailongmiao <2969314242@qq.com>
2025-04-05 20:46:10 +0800

    Use rustfmt --edition 2024 ./file to format Rust code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use axum::{routing::get,Router,extract::State,Json};
-use serde::{Deserialize,Serialize};
+use axum::{extract::State, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPool;
 
 #[derive(Serialize, Deserialize)]
@@ -9,29 +9,24 @@ struct User {
     email: String,
 }
 
-
 #[tokio::main]
 async fn main() {
     let pool = PgPool::connect("postgres://postgres:zhx2004101@localhost:5432/axum_demo")
-    .await
-    .unwrap();
+        .await
+        .unwrap();
     let app = Router::new()
-    .route("/api/users",get(get_users))
-    .with_state(pool);
+        .route("/api/users", get(get_users))
+        .with_state(pool);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-    .await
-    .unwrap();
-    axum::serve(listener,app).await.unwrap();
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_users(State(pool):State<PgPool>)-> Json<Vec<User>>
-{
-    let users = sqlx::query_as!(
-        User,
-        "SELECT id, username, email FROM users"
-    )
-    .fetch_all(&pool)
-    .await
-    .unwrap();
+async fn get_users(State(pool): State<PgPool>) -> Json<Vec<User>> {
+    let users = sqlx::query_as!(User, "SELECT id, username, email FROM users")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
     Json(users)
 }
