d7dc84784282a30e4d92a0314a0963290ae07080
lailongmiao <2969314242@qq.com>
2025-04-10 00:19:33 +0800

    Added register and login, but there is a problem with register

--- migrations/20250405114501_create_users_table.sql
+++ migrations/20250405114501_create_users_table.sql
@@ -1,21 +1,25 @@
-CREATE TABLE users (
-    id serial primary key,
-    username varchar(255) not null unique,
-    email varchar(255) not null unique,
-    team_id int references teams(id),
-    group_id int references groups(id)
-);
-
 CREATE TABLE teams (
     id serial primary key,
     name varchar(255) not null
 );
+
 
 CREATE TABLE groups (
     id serial primary key,
     name varchar(255) not null,
     team_id int references teams(id) not null
 );
+
+
+CREATE TABLE users (
+    id serial primary key,
+    username varchar(255) not null unique,
+    email varchar(255) not null unique,
+    team_id int references teams(id),
+    group_id int references groups(id),
+    password varchar(255) not null
+);
+
 
 INSERT INTO teams(name) VALUES
     ('team1'),
@@ -27,12 +31,13 @@ INSERT INTO groups(name, team_id) VALUES
     ('Group C', 2),
     ('Group D', 2);
 
-INSERT INTO users(id, username, email, team_id, group_id) VALUES
-    (1, '用户测试1', 'test1@example.com', 1, 1),
-    (2, '用户测试2', 'test2@example.com', 1, 1),
-    (3, 'test3', 'test3@example.com', 2, 3),
-    (4, 'test4', 'test4@example.com', 2, 3),
-    (5, 'test5', 'test5@example.com', 2, 4),
-    (6, 'test6', 'test6@example.com', 2, 4),
-    (7, '测试用户7', 'test7@example.com', 1, 2),
-    (8, '测试用户8', 'test8@example.com', 1, 2);
+INSERT INTO users(id, username, email, team_id, group_id,password) VALUES
+    (1, '用户测试1', 'test1@example.com', 1, 1,'123456'),
+    (2, '用户测试2', 'test2@example.com', 1, 1,'123456'),
+    (3, 'test3', 'test3@example.com', 2, 3,'123456'),
+    (4, 'test4', 'test4@example.com', 2, 3,'123456'),
+    (5, 'test5', 'test5@example.com', 2, 4,'123456'),
+    (6, 'test6', 'test6@example.com', 2, 4,'123456'),
+    (7, '测试用户7', 'test7@example.com', 1, 2,'123456'),
+    (8, '测试用户8', 'test8@example.com', 1, 2,'123456');
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use axum::{extract::{State,Path}, routing::get, Json, Router};
+use axum::{extract::{State,Path}, routing::{get,post}, Json, Router};
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPool;
 use tower_http::cors::{Any, CorsLayer};
+use axum::http::StatusCode;
+
 
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -10,6 +12,7 @@ struct User {
     email: String,
     team_id: Option<i32>,
     group_id: Option<i32>,
+    password: String,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -25,9 +28,22 @@ struct Group{
     team_id:i32,
 }
 
+#[derive(Deserialize)]
+struct RegisterInfo{
+    username:String,
+    email:String,
+    password:String,
+}
+
+#[derive(Deserialize)]
+struct LoginInfo{
+    username:String,
+    password:String,
+}
+
 #[tokio::main]
 async fn main() {
-    let pool = PgPool::connect("postgres://postgres:zhx2004101@localhost:5432/axum_demo")
+    let pool = PgPool::connect("postgres://postgres:postgres@localhost:5432/axum_demo")
         .await
         .unwrap();
         
@@ -43,6 +59,8 @@ async fn main() {
         .route("/api/groups",get(get_groups))
         .route("/api/teams/{team_id}/groups",get(get_groups_by_team_id))
         .route("/api/groups/{group_id}/users",get(get_users_by_group_id))
+        .route("/api/register",post(register_user))
+        .route("/api/login",post(login_user))
         .with_state(pool)
         .layer(cors);
         
@@ -53,7 +71,7 @@ async fn main() {
 }
 
 async fn get_users(State(pool): State<PgPool>) -> Json<Vec<User>> {
-    let users = sqlx::query_as!(User, "SELECT id, username, email,team_id,group_id FROM users")
+    let users = sqlx::query_as!(User, "SELECT id, username, email,team_id,group_id,password FROM users")
     .fetch_all(&pool)
     .await
     .unwrap();
@@ -71,7 +89,7 @@ async fn get_teams(State(pool):State<PgPool>) -> Json<Vec<Team>>
 
 async fn get_users_by_team_id_path(State(pool):State<PgPool>,Path(team_id):Path<i32>) ->Json<Vec<User>>
 {
-    let users=sqlx::query_as!(User,"SELECT id,username,email,team_id,group_id from users where team_id=$1",team_id)
+    let users=sqlx::query_as!(User,"SELECT id,username,email,team_id,group_id ,password from users where team_id=$1",team_id)
     .fetch_all(&pool)
     .await
     .unwrap();
@@ -98,10 +116,34 @@ async fn get_groups_by_team_id(State(pool):State<PgPool>,Path(team_id):Path<i32>
 
 async fn get_users_by_group_id(State(pool): State<PgPool>,Path(group_id): Path<i32>) -> Json<Vec<User>> 
 {    
-    let users = sqlx::query_as!(User,"SELECT id, username, email, team_id, group_id FROM users WHERE group_id = $1",group_id)
+    let users = sqlx::query_as!(User,"SELECT id, username, email, team_id, group_id ,password FROM users WHERE group_id = $1",group_id)
     .fetch_all(&pool)
     .await
     .unwrap();
     
     Json(users)
+}
+
+async fn register_user(State(pool):State<PgPool>,Json(register_info):Json<RegisterInfo>)->Result<Json<User>,StatusCode>
+{
+    let new_user=sqlx::query_as!(User,"INSERT INTO users (username,email,password,team_id,group_id) VALUES ($1,$2,$3,$4,$5) RETURNING id,username,email,team_id,group_id,password",register_info.username,register_info.email,register_info.password,Option::<i32>::None,Option::<i32>::None)
+    .fetch_one(&pool)
+    .await
+    .map_err(|_|StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(Json(new_user))
+}
+
+async fn login_user(State(pool):State<PgPool>,Json(login_info):Json<LoginInfo>)->Result<Json<User>,StatusCode>
+{
+    let search_user=sqlx::query_as!(User,"SELECT id,username,email,team_id,group_id,password FROM users WHERE username=$1 AND password=$2",login_info.username,login_info.password)
+    .fetch_optional(&pool)
+    .await
+    .map_err(|_|StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    match search_user
+    {
+        Some(user)=>Ok(Json(user)),
+        None=>Err(StatusCode::UNAUTHORIZED),
+    }
+
 }
