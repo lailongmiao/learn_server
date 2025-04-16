b445a0864f872c5bf4b975d48318add552e59e75
lailongmiao <2969314242@qq.com>
2025-04-17 00:31:37 +0800

    Optimized code but test code has problems

--- migrations/20250405114501_create_users_table.sql
+++ migrations/20250405114501_create_users_table.sql
@@ -1,6 +1,14 @@
-CREATE TABLE teams (
+CREATE TABLE organizations
+(
     id uuid primary key DEFAULT gen_random_uuid(),
     name varchar(255) not null
+);
+
+
+CREATE TABLE teams (
+    id uuid primary key DEFAULT gen_random_uuid(),
+    name varchar(255) not null,
+    organization_id uuid references organizations(id) not null
 );
 
 
@@ -15,18 +23,8 @@ CREATE TABLE users (
     id uuid primary key DEFAULT gen_random_uuid(),
     username varchar(255) not null unique,
     primary_email_address varchar(255) not null unique,
+    organization_id uuid references organizations(id),
     team_id uuid references teams(id),
     group_id uuid references groups(id),
     password varchar(255) not null
 );
-
-
-INSERT INTO teams(id, name) VALUES
-    ('a1111111-1111-1111-1111-111111111111', 'team1'),
-    ('a2222222-2222-2222-2222-222222222222', 'team2');
-
-INSERT INTO groups(id, name, team_id) VALUES
-    ('b1111111-1111-1111-1111-111111111111', 'Group A', 'a1111111-1111-1111-1111-111111111111'),
-    ('b2222222-2222-2222-2222-222222222222', 'Group B', 'a1111111-1111-1111-1111-111111111111'),
-    ('b3333333-3333-3333-3333-333333333333', 'Group C', 'a2222222-2222-2222-2222-222222222222'),
-    ('b4444444-4444-4444-4444-444444444444', 'Group D', 'a2222222-2222-2222-2222-222222222222');
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use axum::{
     response::{IntoResponse, Response},
     routing::{get, post},
 };
+use http::StatusCode;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPool;
@@ -15,17 +16,11 @@ use std::sync::LazyLock;
 use thiserror::Error;
 use tower_http::cors::{Any, CorsLayer};
 use uuid::Uuid;
-use validator::{Validate, ValidationError};
+use validator::Validate;
 
 static ARGON2: LazyLock<Argon2> = LazyLock::new(Argon2::default);
-static LOWERCASE_RE: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r".*[a-z].*").expect("Invalid lowercase regex pattern"));
 static UPPERCASE_RE: LazyLock<Regex> =
     LazyLock::new(|| Regex::new(r".*[A-Z].*").expect("Invalid uppercase regex pattern"));
-
-static DIGIT_RE: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r".*\d.*").expect("Invalid uppercase regex pattern"));
-
 #[derive(Serialize, Deserialize)]
 struct User {
     id: Uuid,
@@ -42,25 +37,37 @@ pub enum ServerError {
     UserNotFound,
     #[error("Password Error")]
     InvalidPassword,
-    #[error("Database Error: {0}")]
-    DatabaseError(#[from] sqlx::Error),
-    #[error(":Registration Error{0}")]
-    RegistrationError(String),
-    #[error("Validation Error : {0}")]
-    ValidationError(#[from] validator::ValidationErrors),
     #[error("Password Hash Error")]
     PasswordHashError,
+    #[error("transparent")]
+    DatabaseError(#[from] sqlx::Error),
+    #[error("transparent")]
+    RegistrationError(String),
+    #[error("transparent")]
+    ValidationError(#[from] validator::ValidationErrors),
 }
 
 impl IntoResponse for ServerError {
     fn into_response(self) -> Response {
         let error_response = match self {
-            ServerError::UserNotFound => "User does not exist".to_string(),
-            ServerError::InvalidPassword => "Password Error".to_string(),
-            ServerError::DatabaseError(err) => format!("Database Error: {}", err),
-            ServerError::RegistrationError(err) => format!("Registration Error: {}", err),
-            ServerError::ValidationError(err) => format!("Validation Error: {}", err),
-            ServerError::PasswordHashError => "Password Hash Error".to_string(),
+            ServerError::UserNotFound => (StatusCode::NOT_FOUND, "User does not exist".to_string()),
+            ServerError::InvalidPassword => (StatusCode::BAD_REQUEST, "Password Error".to_string()),
+            ServerError::DatabaseError(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Database Error: {}", err),
+            ),
+            ServerError::RegistrationError(err) => (
+                StatusCode::UNPROCESSABLE_ENTITY,
+                format!("Registration Error: {}", err),
+            ),
+            ServerError::ValidationError(err) => (
+                StatusCode::UNPROCESSABLE_ENTITY,
+                format!("Validation Error: {}", err),
+            ),
+            ServerError::PasswordHashError => (
+                StatusCode::UNPROCESSABLE_ENTITY,
+                "Password Hash Error".to_string(),
+            ),
         };
         error_response.into_response()
     }
@@ -79,21 +86,31 @@ struct Group {
     team_id: Uuid,
 }
 
-#[derive(Deserialize, Validate, Clone)]
+#[derive(Deserialize, Serialize, Validate)]
 struct RegisterInfo {
-    #[validate(length(min = 1, max = 50))]
+    #[validate(length(
+        min = 3,
+        max = 30,
+        message = "Username must be between 3 and 30 characters long"
+    ))]
     username: String,
-    #[validate(email)]
-    #[validate(length(max = 100))]
+    #[validate(email(message = "Please enter a valid email address"))]
+    #[validate(length(max = 50, message = "Email address length cannot exceed 50 characters"))]
     email: String,
-    #[validate(length(min = 6))]
-    #[validate(custom(function = "validate_password"))]
+    #[validate(length(min = 6, message = "The password must be at least 6 characters"))]
+    #[validate(regex(path=*UPPERCASE_RE,message = "passwords must contain at least one upper case letter"))]
     password: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize, Validate)]
 struct LoginInfo {
+    #[validate(length(
+        min = 3,
+        max = 30,
+        message = "The Username does not conform to validation rules"
+    ))]
     username: String,
+    #[validate(length(min = 6, message = "The password does not meet the verification rules"))]
     password: String,
 }
 
@@ -254,6 +271,7 @@ async fn login_user(
     State(pool): State<PgPool>,
     Json(login_info): Json<LoginInfo>,
 ) -> Result<Json<User>, ServerError> {
+    login_info.validate()?;
     let search_user = sqlx::query_as!(
         User,
         r#"
@@ -287,39 +305,42 @@ WHERE username=$1
     }
 }
 
-fn validate_password(password: &str) -> Result<(), ValidationError> {
-    let lowercase = LOWERCASE_RE.is_match(password);
-    let uppercase = UPPERCASE_RE.is_match(password);
-    let digit = DIGIT_RE.is_match(password);
-
-    if lowercase && uppercase && digit {
-        Ok(())
-    } else {
-        let error = ValidationError::new("password_error");
-        Err(error)
-    }
-}
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
     use sqlx::postgres::PgPool;
-    #[tokio::test]
-    async fn test_register_user()
-    {
-        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    use tower::ServiceExt;
 
+    async fn create_test_app() -> Router {
+        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         let pool = PgPool::connect(&database_url).await.unwrap();
-        let register_info =RegisterInfo{
-            username: "test_user".to_string(),
-            email:"testuser@example.com".to_string(),
-            password:"Password123!".to_string(),
+        Router::new()
+            .route("/api/users", get(get_users))
+            .with_state(pool)
+    }
+
+    #[tokio::test]
+    async fn test_register_user_success() {
+        let app = create_test_app().await;
+        let register_info = RegisterInfo {
+            username: "haoxiangzhou".to_string(),
+            email: "haoxiangzhou@example.com".to_string(),
+            password: "P2025zhx".to_string(),
         };
-        let result =register_user(State(pool),Json(register_info)).await;
-        match result{
-            Ok(_)=>println!("User registered successfully"),
-            Err(ref e)=>println!("Failed to register user: {}",e),
-        }
-        assert!(result.is_ok());
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/api/register")
+                    .method("POST")
+                    .body(Body::from(serde_json::to_string(&register_info).unwrap()))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
     }
 }
